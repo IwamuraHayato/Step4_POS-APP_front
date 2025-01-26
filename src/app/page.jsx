a277c92074c5a23c9aa93feadaec79053d737777
Hayato Iwamura <8810.com@gmail.com>
2025-01-26 13:18:06 +0900

    POSーAPP構築開始

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,48 @@
+'use client';
+import { useState } from 'react';
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  const [itemCode, setItemCode] = useState('');
+  const [postResult, setPostResult] = useState('');
+  const handlePostRequest = async () => {
+    try{
+      const response = await fetch('http://127.0.0.1:8000/api/read', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ itemCode }),
+      });
+      const data = await response.json();
+    setPostResult(data.message);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  
+  return (
+    <div className="p-8">
+      <section>
+        <h1>商品コードを入力してください。</h1>
+        <div className="flex gap-2">
+          <input 
+          type="text" 
+          value={itemCode} 
+          onChange={(e) => setItemCode(e.target.value)} 
+          className="border-2 border-indigo-600 rounded px-2 py-1"
+          />
+          <button 
+          className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+          onClick={handlePostRequest}>商品コード 読み込み
+          </button>
+        </div>
+        {postResult && (
+            <p className="mt-2"> {postResult}</p>
+          )}
+      </section>
+      <section>
+
+      </section>
+    </div>
+  );
 }
